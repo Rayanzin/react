48a96b7f5a11b3507e0ab3fad6c5857f5f87336b
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-12-09 11:53:51 -0300

    introdução ao react

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+const BoasVindas = (props) => {
+  return (
+    <h1>Seja Bem-Vind{props.genero == 'f' ? 'a' : 'o'} {props.nome}</h1>
+  )
+}
+
+
+const App = () => {
+  const alunos = [
+    {
+      nome: "Levi",
+      genero: "m"
+    },
+    {
+      nome: "Alexandre",
+      genero: "m"
+    },
+    {
+      nome: "Nayara",
+      genero: "f"
+    },
+    {
+      nome: "Liana",
+      genero: "f"
+    }
+  ]
+  return (
+    <>
+      {
+        alunos.map(aluno => (
+          <BoasVindas nome={aluno.nome} genero={aluno.genero} />
+        ))
+      }
+    </>
+  )
+}
+
+export default App;
