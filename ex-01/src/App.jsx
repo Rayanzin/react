95c0e76715fbd48c24c12bb3c717335a70d45993
Rayan <139246019+Rayanzin@users.noreply.github.com>
2024-12-13 00:07:33 -0300

    estado de um componente

--- ex-01/src/App.jsx
+++ ex-01/src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react";
 
 
 const App = () => {
-  let message = 'Olá, Mundo!'
+  const [mensagem, novaMensagem] = useState("Adicione dois numeros e calcule.")
   return (
     <>
-      <h1>{message}</h1>
-      <button {onClick></button>
+      <h1>{mensagem}</h1>
+      <input id="n1" type="number" />
+      <input id="n2" type="number" />
+      <button onClick={() => {
+        novaMensagem(Number(n1.value) + Number(n2.value))
+      }}>Calcular</button>
     </>
   )
 }
