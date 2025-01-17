313cc077f401b6d5799b219ec1977eb7cf80e78f
Rayan <139246019+Rayanzin@users.noreply.github.com>
2025-01-17 19:14:46 -0300

    tudo indo ladeira a baixo

--- exercicios/motorista-react/src/pages/Home.jsx
+++ exercicios/motorista-react/src/pages/Home.jsx
@@ -3,21 +3,21 @@ import Header from "../components/Header";
 
 const Home = () => {
     return ( 
-        <div className="text-white w-screen h-screen bg-[#3b3b3b] ">
+        <div className=" text-white w-screen h-screen bg-[#3b3b3b] ">
             <div className="flex flex-col items-center ">
                 <Header/>
-                <table className="broder-2 border-white mt-10 flex flex-col border-collapse bg-[#555] p-2 w-auto justify-center rounded-md">
-                    <tr className="flex items-center gap-20">
-                        <th>Nome</th>
-                        <th>Estado</th>
-                        <th>
+                <table className=" mt-10 flex flex-col border-collapse bg-[#555] p-2 w-auto justify-center rounded-md">
+                    <tr className="flex items-center">
+                        <th className="border-2 border-white">Nome</th>
+                        <th className="border-2 border-white">Estado</th>
+                        <th className="border-2 border-white">
                             <FixedButton/>
                         </th>
                     </tr>
-                    <tr className="flex justify-center items-center">
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
+                    <tr className="flex justify-center items-center ">
+                        <td className="border-2 border-white">1</td>
+                        <td className="border-2 border-white">2</td>
+                        <td className="border-2 border-white">3</td>
                     </tr>
                 </table>
             </div>
